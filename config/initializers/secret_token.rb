37d130e8b1ea73c9da8af32a8c45eed034b151a8
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-06-06 10:25:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaniTools::Application.config.secret_token = 'cb7bc4590513f7a20dcf05b69398d4c3805f41de1b09e2d2eeee2b3f8fcdf55884790fa82870a110f8e3c6e761dffe8c2051ffe5a7f5ead38397f21524908475'
